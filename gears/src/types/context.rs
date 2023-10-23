32e857c95bb6bf0052170a4c172b3185b4ff9440
NYBACHOK <nybachokua@gmail.com>
2023-10-23 15:36:08 +0300

    Use of Arc in Context's

--- gears/src/types/context.rs
+++ gears/src/types/context.rs
@@ -1,9 +1,14 @@
+use std::sync::{Arc, RwLock};
+
 use database::{Database, PrefixDB};
+use ibc_relayer::util::lock::LockExt;
 use tendermint_informal::{abci::Event, block::Header};
 
 use store_crate::{KVStore, MultiStore, QueryKVStore, QueryMultiStore, StoreKey};
 
 use crate::error::AppError;
+
+use super::gas::{gas_meter::GasMeter, infinite_meter::InfiniteGasMeter};
 
 /// Execution mode of transaction
 #[derive(Debug, PartialEq)]
@@ -24,131 +29,183 @@ pub enum ExecMode {
     ExecModeFinalize,
 }
 
-pub struct TxContext<'a, T: Database, SK: StoreKey> {
-    multi_store: &'a mut MultiStore<T, SK>,
+pub type MS<T: Database, SK: StoreKey> = Arc<RwLock<MultiStore<T, SK>>>;
+
+pub trait ContextTrait<T: Database, SK: StoreKey> {
+    fn gas_meter(&self) -> &dyn GasMeter;
+    fn block_gas_meter(&self) -> &dyn GasMeter;
+    fn gas_meter_mut(&mut self) -> &mut dyn GasMeter;
+    fn block_gas_meter_mut(&mut self) -> &mut dyn GasMeter;
+    // fn as_any<'b>(&'b mut self) -> Context<T, SK>; //TODO:
+    // ///  Fetches an immutable ref to a KVStore from the MultiStore.
+    // fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>>;
+    // /// Fetches a mutable ref to a KVStore from the MultiStore.
+    // fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>>;
+    fn get_height(&self) -> u64;
+    fn push_event(&mut self, event: Event);
+    fn append_events(&mut self, events: Vec<Event>);
+}
+
+impl<'a, T: Database, SK: StoreKey> ContextTrait<T, SK> for TxContext<T, SK> {
+    fn gas_meter(&self) -> &dyn GasMeter {
+        &self.gas_meter
+    }
+
+    fn block_gas_meter(&self) -> &dyn GasMeter {
+        &self.block_gas_meter
+    }
+
+    fn gas_meter_mut(&mut self) -> &mut dyn GasMeter {
+        &mut self.gas_meter
+    }
+
+    fn block_gas_meter_mut(&mut self) -> &mut dyn GasMeter {
+        &mut self.block_gas_meter
+    }
+
+    // fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>> {
+    //     self.multi_store.acquire_write().get_kv_store(store_key)
+    // }
+
+    // fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>> {
+    //     self.multi_store.acquire_write().get_mutable_kv_store(store_key)
+    // }
+
+    fn get_height(&self) -> u64 {
+        self.height
+    }
+
+    fn push_event(&mut self, event: Event) {
+        self.events.push(event)
+    }
+
+    fn append_events(&mut self, mut events: Vec<Event>) {
+        self.events.append(&mut events)
+    }
+}
+
+pub struct TxContext<T: Database, SK: StoreKey> {
+    multi_store: MS<T, SK>,
     height: u64,
     pub events: Vec<Event>,
     header: Header,
     _tx_bytes: Vec<u8>,
-    // gas_meter             : GasMeter,
-    // block_gas_meter        : GasMeter,
+    gas_meter: InfiniteGasMeter,       //TODO: Trait
+    block_gas_meter: InfiniteGasMeter, //TODO: Trait
 }
 
-impl<'a, T: Database, SK: StoreKey> TxContext<'a, T, SK> {
-    pub fn new(
-        multi_store: &'a mut MultiStore<T, SK>,
-        height: u64,
-        header: Header,
-        tx_bytes: Vec<u8>,
-    ) -> Self {
+impl<T: Database, SK: StoreKey> TxContext<T, SK> {
+    pub fn new(multi_store: MS<T, SK>, height: u64, header: Header, tx_bytes: Vec<u8>) -> Self {
         TxContext {
             multi_store,
             height,
             events: vec![],
             header,
             _tx_bytes: tx_bytes,
+            gas_meter: InfiniteGasMeter::new(),
+            block_gas_meter: InfiniteGasMeter::new(),
         }
     }
 
-    pub fn get_header(&self) -> &Header {
+    fn get_header(&self) -> &Header {
         &self.header
     }
 
-    //TODO: implement From on Context
-    pub fn as_any<'b>(&'b mut self) -> Context<'b, 'a, T, SK> {
-        Context::TxContext(self)
-    }
-
-    ///  Fetches an immutable ref to a KVStore from the MultiStore.
-    pub fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>> {
-        return self.multi_store.get_kv_store(store_key);
-    }
-
-    /// Fetches a mutable ref to a KVStore from the MultiStore.
-    pub fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>> {
-        return self.multi_store.get_mutable_kv_store(store_key);
-    }
-
-    pub fn get_height(&self) -> u64 {
-        self.height
-    }
-
-    pub fn push_event(&mut self, event: Event) {
-        self.events.push(event);
-    }
-
-    pub fn append_events(&mut self, mut events: Vec<Event>) {
-        self.events.append(&mut events);
+    fn multi_store(&self) -> &MS<T, SK> {
+        &self.multi_store
     }
 }
 
-pub struct InitContext<'a, T: Database, SK: StoreKey> {
-    pub multi_store: &'a mut MultiStore<T, SK>,
+pub struct InitContext<T: Database, SK: StoreKey> {
+    pub multi_store: MS<T, SK>,
     height: u64,
     pub events: Vec<Event>,
     pub chain_id: String,
+    gas_meter: InfiniteGasMeter,       //TODO: Trait
+    block_gas_meter: InfiniteGasMeter, //TODO: Trait
 }
 
-impl<'a, T: Database, SK: StoreKey> InitContext<'a, T, SK> {
-    pub fn new(multi_store: &'a mut MultiStore<T, SK>, height: u64, chain_id: String) -> Self {
+impl<'a, T: Database, SK: StoreKey> InitContext<T, SK> {
+    pub fn new(multi_store: MS<T, SK>, height: u64, chain_id: String) -> Self {
         InitContext {
             multi_store,
             height,
             events: vec![],
             chain_id,
+            gas_meter: InfiniteGasMeter::new(),
+            block_gas_meter: InfiniteGasMeter::new(),
         }
     }
 
-    //TODO: implement From on Context
-    pub fn as_any<'b>(&'b mut self) -> Context<'b, 'a, T, SK> {
-        Context::InitContext(self)
+    pub fn multi_store(&self) -> &MS<T, SK> {
+        &self.multi_store
     }
 
-    ///  Fetches an immutable ref to a KVStore from the MultiStore.
-    pub fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>> {
-        return self.multi_store.get_kv_store(store_key);
+    // fn as_any(&self) -> Context<T, SK> {
+    //     Context::InitContext(self)
+    // }
+}
+
+impl<'a, T: Database, SK: StoreKey> ContextTrait<T, SK> for InitContext<T, SK> {
+    fn gas_meter(&self) -> &dyn GasMeter {
+        &self.gas_meter
     }
 
-    /// Fetches a mutable ref to a KVStore from the MultiStore.
-    pub fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>> {
-        return self.multi_store.get_mutable_kv_store(store_key);
+    fn block_gas_meter(&self) -> &dyn GasMeter {
+        &self.block_gas_meter
     }
 
-    pub fn get_height(&self) -> u64 {
+    fn gas_meter_mut(&mut self) -> &mut dyn GasMeter {
+        &mut self.gas_meter
+    }
+
+    fn block_gas_meter_mut(&mut self) -> &mut dyn GasMeter {
+        &mut self.block_gas_meter
+    }
+
+    // fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>> {
+    //     self.multi_store.acquire_write().get_kv_store(store_key)
+    // }
+
+    // fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>> {
+    //     self.multi_store.acquire_write().get_mutable_kv_store(store_key)
+    // }
+
+    fn get_height(&self) -> u64 {
         self.height
     }
 
-    pub fn push_event(&mut self, event: Event) {
-        self.events.push(event);
+    fn push_event(&mut self, event: Event) {
+        self.events.push(event)
     }
 
-    pub fn append_events(&mut self, mut events: Vec<Event>) {
-        self.events.append(&mut events);
+    fn append_events(&mut self, mut events: Vec<Event>) {
+        self.events.append(&mut events)
     }
 }
 
 /// This is used when a method can be used in either a tx or init context
-pub enum Context<'a, 'b, T: Database, SK: StoreKey> {
-    TxContext(&'a mut TxContext<'b, T, SK>),
-    InitContext(&'a mut InitContext<'b, T, SK>),
+pub enum Context<'a, T: Database, SK: StoreKey> {
+    TxContext(&'a mut TxContext<T, SK>),
+    InitContext(&'a mut InitContext<T, SK>),
 }
 
-impl<'a, 'b, T: Database, SK: StoreKey> Context<'a, 'b, T, SK> {
-    ///  Fetches an immutable ref to a KVStore from the MultiStore.
-    pub fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>> {
-        match self {
-            Context::TxContext(ctx) => return ctx.get_kv_store(store_key),
-            Context::InitContext(ctx) => return ctx.multi_store.get_kv_store(store_key),
-        }
-    }
+impl<'a, T: Database, SK: StoreKey> Context<'a, T, SK> {
+    // ///  Fetches an immutable ref to a KVStore from the MultiStore.
+    // pub fn get_kv_store(&self, store_key: &SK) -> &KVStore<PrefixDB<T>> {
+    //     match self {
+    //         Context::TxContext(ctx) => return ctx.get_kv_store(store_key),
+    //         Context::InitContext(ctx) => return ctx.multi_store.acquire_read().get_kv_store(store_key),
+    //     }
+    // }
 
-    /// Fetches a mutable ref to a KVStore from the MultiStore.
-    pub fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>> {
-        match self {
-            Context::TxContext(ctx) => return ctx.get_mutable_kv_store(store_key),
-            Context::InitContext(ctx) => return ctx.multi_store.get_mutable_kv_store(store_key),
-        }
-    }
+    // /// Fetches a mutable ref to a KVStore from the MultiStore.
+    // pub fn get_mutable_kv_store(&mut self, store_key: &SK) -> &mut KVStore<PrefixDB<T>> {
+    //     match self {
+    //         Context::TxContext(ctx) => return ctx.get_mutable_kv_store(store_key),
+    //         Context::InitContext(ctx) => return ctx.multi_store.acquire_write().get_mutable_kv_store(store_key),
+    //     }
+    // }
 
     pub fn get_height(&self) -> u64 {
         match self {
@@ -176,6 +233,43 @@ impl<'a, 'b, T: Database, SK: StoreKey> Context<'a, 'b, T, SK> {
             Context::TxContext(ctx) => ctx.append_events(events),
             Context::InitContext(ctx) => ctx.events.append(&mut events),
         }
+    }
+
+    pub fn gas_meter(&self) -> &dyn GasMeter {
+        match self {
+            Context::TxContext(ctx) => ctx.gas_meter(),
+            Context::InitContext(ctx) => ctx.gas_meter(),
+        }
+    }
+
+    pub fn block_gas_meter(&self) -> &dyn GasMeter {
+        match self {
+            Context::TxContext(ctx) => ctx.block_gas_meter(),
+            Context::InitContext(ctx) => ctx.block_gas_meter(),
+        }
+    }
+
+    pub fn multi_store(&self) -> &MS<T, SK> {
+        match self {
+            Context::TxContext(ctx) => ctx.multi_store(),
+            Context::InitContext(ctx) => ctx.multi_store(),
+        }
+    }
+
+    pub fn with_multi_store(&self) -> Self {
+        unimplemented!() //TODO
+    }
+}
+
+impl<'a, T: Database, SK: StoreKey> From<&'a mut TxContext<T, SK>> for Context<'a, T, SK> {
+    fn from(value: &'a mut TxContext<T, SK>) -> Self {
+        Self::TxContext(value)
+    }
+}
+
+impl<'a, T: Database, SK: StoreKey> From<&'a mut InitContext<T, SK>> for Context<'a, T, SK> {
+    fn from(value: &'a mut InitContext<T, SK>) -> Self {
+        Self::InitContext(value)
     }
 }
 
