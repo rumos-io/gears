bd6975feeb22f5cb0c7739ef545153fb48adaacc
NYBACHOK <nybachokua@gmail.com>
2024-01-12 15:28:10 +0200

    remove dbg macro

--- x/auth/src/signing/hasher.rs
+++ x/auth/src/signing/hasher.rs
@@ -4,8 +4,6 @@ use sha2::Sha256;
 /// `hash_get` gets the hash of raw bytes to be signed over:
 /// HEX(sha256(len(body_bytes) ++ body_bytes ++ len(auth_info_bytes) ++ auth_info_bytes))
 pub fn hash_get(body_bytes: &Vec< u8 >, auth_info_bytes: &Vec<u8>) -> String {
-
-
     let mut buffer = Vec::<u8>::with_capacity(16 + body_bytes.len() + auth_info_bytes.len());
     let body_len = (body_bytes.len() as u64 ).to_be_bytes();
     let auth_info_len = (auth_info_bytes.len() as u64 ).to_be_bytes();
@@ -20,11 +18,7 @@ pub fn hash_get(body_bytes: &Vec< u8 >, auth_info_bytes: &Vec<u8>) -> String {
 
     let finalize = Sha256::digest( buffer );
 
-    let f = hex::encode(finalize);
-
-    dbg!( &f );
-
-    f
+    hex::encode(finalize)
 }
 
 // pub fn get_hash(body_bz: &[u8], auth_info_bz: &[u8]) -> String {
