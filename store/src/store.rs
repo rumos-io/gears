f81aada1b1e3a311236728a46d04d6e52d1be101
NYBACHOK <nybachokua@gmail.com>
2024-02-28 16:36:39 +0200

    delete deadcode

--- store/src/store.rs
+++ store/src/store.rs
@@ -150,23 +150,7 @@ impl<DB: Database> KVStore<DB> {
             tx_cache: BTreeMap::new(),
         })
     }
-
-    pub fn get(&self, key: impl AsRef<[u8]>) -> Option<Vec<u8>> {
-        let tx_cache_val = self.tx_cache.get(key.as_ref());
-
-        if tx_cache_val.is_none() {
-            let block_cache_val = self.block_cache.get(key.as_ref());
-
-            if block_cache_val.is_none() {
-                return self.persistent_store.get(key.as_ref());
-            };
-
-            return block_cache_val.cloned();
-        }
-
-        tx_cache_val.cloned()
-    }
-
+    
     pub fn get_immutable_prefix_store(
         &self,
         prefix: impl IntoIterator<Item = u8>,
