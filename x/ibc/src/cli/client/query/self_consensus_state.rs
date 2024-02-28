c3791875c10108fb9e43102505af5791d4434dac
NYBACHOK <nybachokua@gmail.com>
2024-02-28 13:44:30 +0200

    small additions

--- x/ibc/src/cli/client/query/self_consensus_state.rs
+++ x/ibc/src/cli/client/query/self_consensus_state.rs
@@ -1,5 +1,6 @@
 use clap::Args;
 use gears::types::context::query_context::QueryContext;
+use tendermint::informal::block::Height;
 
 #[derive(Args, Debug, Clone)]
 pub struct CliClientParams;
@@ -7,7 +8,9 @@ pub struct CliClientParams;
 #[allow(dead_code)]
 pub(super) fn query_command_handler<DB, SK>(
     _ctx: &QueryContext<'_, DB, SK>,
-    _msg: &CliClientParams,
+    _args: CliClientParams,
+    _node: &str,
+    _height: Option<Height>,
 ) -> anyhow::Result<String> {
     Ok(String::new())
 }
