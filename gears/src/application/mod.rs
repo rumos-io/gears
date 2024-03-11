a5d3730fe5d98b01173dd0c92ebdcdcfd783f353
NYBACHOK <nybachokua@gmail.com>
2024-03-11 16:30:27 +0200

    remove unused structs

--- gears/src/application/mod.rs
+++ gears/src/application/mod.rs
@@ -13,14 +13,3 @@ pub trait ApplicationInfo: Clone + Sync + Send + 'static {
             .join(format!(".{}", Self::APP_NAME)) // TODO: what about using version as prefix?
     }
 }
-
-#[derive(Debug, Clone)]
-pub struct DefaultApplication;
-
-impl ApplicationInfo for DefaultApplication {
-    const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
-    const APP_VERSION: &'static str = "1"; // TODO: GIT_HASH
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct ApplicationBuilder;
