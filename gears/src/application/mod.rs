36d8574f57ea69932f110cb5b1a788beb84bbf87
NYBACHOK <nybachokua@gmail.com>
2024-03-11 16:33:46 +0200

    default for info

--- gears/src/application/mod.rs
+++ gears/src/application/mod.rs
@@ -4,8 +4,8 @@ pub mod command;
 pub mod handlers;
 
 pub trait ApplicationInfo: Clone + Sync + Send + 'static {
-    const APP_NAME: &'static str;
-    const APP_VERSION: &'static str;
+    const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+    const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
     fn home_dir() -> std::path::PathBuf {
         dirs::home_dir()
