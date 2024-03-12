ce7884ac9c1ce856c4f8f5a33760d1bc4d133c23
NYBACHOK <nybachokua@gmail.com>
2024-03-12 17:13:35 +0200

    query handler v2 without impl

--- gears/src/application/handlers_v2.rs
+++ gears/src/application/handlers_v2.rs
@@ -1,9 +1,23 @@
+use prost::Message;
+use proto_messages::cosmos::ibc::protobuf::Protobuf;
 use proto_messages::cosmos::query::QueryUrl;
-use tendermint::informal::block::Height;
+use serde::Serialize;
+use tendermint::{
+    informal::block::Height,
+    rpc::{Client, HttpClient},
+};
 
-pub trait QueryHandler {
-    type Query: QueryUrl;
-    type QueryResponse;
+use crate::runtime::runtime;
+
+pub trait QueryHandler
+where
+    <Self::QueryResponse as TryFrom<Self::RawQueryResponse>>::Error: std::fmt::Display,
+{
+    type Query: QueryUrl + Message;
+    type RawQueryResponse: Message + Default + std::convert::From<Self::QueryResponse>;
+    type QueryResponse: Protobuf<Self::RawQueryResponse>
+        + std::convert::TryFrom<Self::RawQueryResponse>
+        + Serialize;
     type QueryCommand;
 
     fn prepare_query(
@@ -13,5 +27,26 @@ pub trait QueryHandler {
         height: Option<Height>,
     ) -> anyhow::Result<Self::Query>;
 
-    fn handle_query(&self) -> anyhow::Result<Self::QueryResponse>; // TODO: default impl?
+    fn handle_query(
+        &self,
+        query: Self::Query,
+        path: String,
+        node: &str,
+        height: Option<Height>,
+    ) -> anyhow::Result<Self::QueryResponse> {
+        let client = HttpClient::new(node)?;
+
+        let res = runtime().block_on(client.abci_query(
+            Some(path),
+            query.encode_to_vec(),
+            height,
+            false,
+        ))?;
+
+        if res.code.is_err() {
+            return Err(anyhow::anyhow!("node returned an error: {}", res.log));
+        }
+
+        Self::QueryResponse::decode(&*res.value).map_err(|e| e.into())
+    }
 }
