3caf0ff8edc484f27f6110f05ab40d2cff993730
NYBACHOK <nybachokua@gmail.com>
2024-05-16 16:51:49 +0300

    tests for cache struct

--- store/src/types/kv/store_cache.rs
+++ store/src/types/kv/store_cache.rs
@@ -41,3 +41,68 @@ impl KVCache {
         self.storage.insert(key, value.into_iter().collect());
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::KVCache;
+
+    #[test]
+    fn delete_removes_insert() {
+        let mut cache = KVCache::default();
+
+        cache.storage.insert(vec![1], vec![2]);
+        cache.storage.insert(vec![3], vec![4]);
+
+        // ---
+        cache.delete([1].as_slice());
+
+        // ---
+        assert!(!cache.storage.contains_key([1].as_slice()));
+        assert!(cache.storage.contains_key([3].as_slice()));
+        assert!(cache.delete.contains([1].as_slice()));
+    }
+
+    #[test]
+    fn set_removes_delete() {
+        let mut cache = KVCache::default();
+
+        cache.delete.insert(vec![1]);
+
+        // ---
+        cache.set(vec![1], vec![2]);
+
+        // ---
+        assert!(cache.storage.contains_key([1].as_slice()));
+        assert!(!cache.delete.contains([1].as_slice()));
+    }
+
+    #[test]
+    fn set_overrides() {
+        let mut cache = KVCache::default();
+
+        cache.storage.insert(vec![1], vec![2]);
+
+        // ---
+        cache.set(vec![1], vec![3]);
+
+        // ---
+        assert!(cache.storage.contains_key([1].as_slice()));
+        assert_eq!(Some(&vec![3]), cache.storage.get([1].as_slice()));
+    }
+
+    #[test]
+    fn deleted_not_gets() {
+        let mut cache = KVCache::default();
+
+        cache.storage.insert(vec![1], vec![2]);
+        cache.delete.insert(vec![1]);
+
+        // ---
+
+        let result = cache.get(&[1]);
+
+        //
+        assert_eq!(None, result);
+    }
+}
