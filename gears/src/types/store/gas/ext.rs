61cb8712d062138fb9564cd4dfb0a63fb2b4dd3c
NYBACHOK <nybachokua@gmail.com>
2024-06-04 16:04:58 +0300

    add unwrap method for it

---
+++ gears/src/types/store/gas/ext.rs
@@ -0,0 +1,17 @@
+use super::errors::GasStoreErrors;
+
+pub const NO_GAS_IN_CTX: &str = "Context shouldn't have any gas so it save to unwrap";
+
+pub trait GasResultExt {
+    type Output;
+
+    fn unwrap_gas(self) -> Self::Output;
+}
+
+impl<T> GasResultExt for Result<T, GasStoreErrors> {
+    type Output = T;
+
+    fn unwrap_gas(self) -> Self::Output {
+        self.expect(NO_GAS_IN_CTX)
+    }
+}
