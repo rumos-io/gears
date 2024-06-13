3a964a7fd80b9040fc7c2e96fce7fb70e640d59d
dmvict <dm.vict.kr@gmail.com>
2024-06-13 08:32:22 +0300

    fix minor issues with helper for creating module names

--- x/auth/src/lib.rs
+++ x/auth/src/lib.rs
@@ -16,11 +16,10 @@ pub use params::*;
 
 pub fn new_module_addr(module_name: &str) -> gears::types::address::AccAddress {
     use sha2::Digest;
-    let mut hasher = sha2::Sha256::new();
-    hasher.update(module_name.as_bytes());
+    let hash = sha2::Sha256::digest(module_name);
     // sdk behavior. It gets slice of first 20 bytes from sha256 hash
-    let result = &hasher.finalize()[..20];
+    let addr_bytes = &hash[..20];
 
-    // SAFETY: vector of 20 bytes can't produce error because 0 < 20 < MAX_ADDR_LEN
-    gears::types::address::AccAddress::try_from(result.to_vec()).unwrap()
+    gears::types::address::AccAddress::try_from(addr_bytes.to_vec())
+        .expect("vector of 20 bytes can't produce error because 0 < 20 < MAX_ADDR_LEN")
 }
