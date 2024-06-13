3d8634944bfc9b50a0ebc33003be46d124d2be19
NYBACHOK <nybachokua@gmail.com>
2024-06-13 11:59:42 +0300

    finish with add deposit

--- x/gov/src/keeper.rs
+++ x/gov/src/keeper.rs
@@ -3,13 +3,17 @@ use std::marker::PhantomData;
 use anyhow::anyhow;
 use gears::{
     application::keepers::params::ParamsKeeper,
-    context::{init::InitContext, tx::TxContext},
+    context::{init::InitContext, tx::TxContext, TransactionalContext},
     params::ParamsSubspaceKey,
     store::{database::Database, StoreKey},
-    types::store::gas::{
-        errors::GasStoreErrors,
-        ext::GasResultExt,
-        kv::{mutable::GasKVStoreMut, GasKVStore},
+    tendermint::types::proto::event::{Event, EventAttribute},
+    types::{
+        address::AccAddress,
+        store::gas::{
+            errors::GasStoreErrors,
+            ext::GasResultExt,
+            kv::{mutable::GasKVStoreMut, GasKVStore},
+        },
     },
     x::{keepers::bank::BankKeeper, module::Module},
 };
@@ -157,12 +161,12 @@ impl<SK: StoreKey, PSK: ParamsSubspaceKey, BM: Module, BK: BankKeeper<SK, BM>>
 
         self.bank_keeper.send_coins_from_account_to_module(
             ctx,
-            depositor,
+            depositor.clone(),
             &self.gov_mod,
             amount.clone(),
         )?;
 
-        proposal.total_deposit = proposal.total_deposit.checked_add(amount)?;
+        proposal.total_deposit = proposal.total_deposit.checked_add(amount.clone())?;
         proposal_set(ctx.kv_store_mut(&self.store_key), &proposal)?;
 
         let deposit_params = self.gov_params_keeper.try_get(ctx)?.deposit;
@@ -176,6 +180,38 @@ impl<SK: StoreKey, PSK: ParamsSubspaceKey, BM: Module, BK: BankKeeper<SK, BM>>
             }
             _ => false,
         };
+
+        let deposit = match deposit_get(ctx.kv_store(&self.store_key), proposal_id, &depositor)? {
+            Some(mut deposit) => {
+                deposit.amount = deposit.amount.checked_add(amount)?;
+                deposit
+            }
+            None => Deposit {
+                proposal_id,
+                depositor,
+                amount,
+            },
+        };
+
+        // TODO: ADD HOOK https://github.com/cosmos/cosmos-sdk/blob/d3f09c222243bb3da3464969f0366330dcb977a8/x/gov/keeper/deposit.go#L149
+
+        ctx.push_event(Event::new(
+            "proposal_deposit",
+            vec![
+                EventAttribute::new(
+                    "amount".into(),
+                    format!("{:?}", deposit.amount).into(),
+                    false,
+                ),
+                EventAttribute::new(
+                    "proposal_id".into(),
+                    format!("{}", deposit.proposal_id).into(),
+                    false,
+                ),
+            ],
+        ));
+
+        deposit_set(ctx.kv_store_mut(&self.store_key), &deposit)?;
 
         Ok(activated_voting_period)
     }
@@ -203,7 +239,37 @@ fn proposal_set<DB: Database>(
     store.set(
         proposal.key(),
         serde_json::to_vec(proposal).expect(SERDE_JSON_CONVERSION),
-    )?;
+    )
+}
 
-    Ok(())
+fn deposit_get<DB: Database>(
+    store: GasKVStore<'_, DB>,
+    proposal_id: u64,
+    depositor: &AccAddress,
+) -> Result<Option<Deposit>, GasStoreErrors> {
+    let key = [
+        KEY_DEPOSIT_PREFIX.as_slice(),
+        &proposal_id.to_be_bytes(),
+        &[depositor.len()],
+        depositor.as_ref(),
+    ]
+    .concat();
+
+    let bytes = store.get(&key)?;
+    match bytes {
+        Some(var) => Ok(Some(
+            serde_json::from_slice(&var).expect(SERDE_JSON_CONVERSION),
+        )),
+        None => Ok(None),
+    }
+}
+
+fn deposit_set<DB: Database>(
+    mut store: GasKVStoreMut<'_, DB>,
+    deposit: &Deposit,
+) -> Result<(), GasStoreErrors> {
+    store.set(
+        deposit.key(),
+        serde_json::to_vec(deposit).expect(SERDE_JSON_CONVERSION),
+    )
 }
