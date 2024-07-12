a9ec7e69b2af9c149a2f34d1d9b278bac0f43efe
NYBACHOK <nybachokua@gmail.com>
2024-07-12 17:01:43 +0300

    implement head version and hash for query ms

--- x/gov/src/submission/handler.rs
+++ x/gov/src/submission/handler.rs
@@ -43,7 +43,7 @@ impl<PSK: ParamsSubspaceKey, PK: ParamsKeeper<PSK>> SubmissionHandler<PK, PSK, P
 
         let mut store = subspace_mut(ctx, subspace_key);
 
-        // This block is still safe, but I use it to make sure to indicate that this method could break any 
+        // This block is still safe, but I use it to make sure to indicate that this method could break any
         unsafe {
             store.raw_key_set(proposal.key, proposal.value)?;
         }
