c38f729cde153c4d232d99344909bebf2b020a01
NYBACHOK <nybachokua@gmail.com>
2024-07-15 13:38:23 +0300

    return old pagination for rest

--- gears/src/rest/pagination.rs
+++ gears/src/rest/pagination.rs
@@ -1,24 +1,21 @@
 use crate::types::pagination::request::{PaginationRequest, QUERY_DEFAULT_LIMIT};
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, Eq, PartialEq)]
+#[serde(default)]
 pub struct Pagination {
     /// offset is a numeric offset that can be used when key is unavailable.
     /// It is less efficient than using key. Only one of offset or key should
     /// be set.
-    pub offset: u32,
+    offset: Option<u32>,
     /// limit is the total number of results to be returned in the result page.
     /// If left empty it will default to a value to be set by each app.
-    pub limit: u8,
-}
-
-impl From<PaginationRequest> for Pagination {
-    fn from(PaginationRequest { offset, limit }: PaginationRequest) -> Self {
-        Self { offset, limit }
-    }
+    limit: Option<u8>,
 }
 
 impl From<Pagination> for PaginationRequest {
-    fn from(Pagination { offset, limit }: Pagination) -> Self {
+    fn from(pagination: Pagination) -> Self {
+        let (offset, limit) = parse_pagination(pagination);
+
         Self { offset, limit }
     }
 }
@@ -26,14 +23,17 @@ impl From<Pagination> for PaginationRequest {
 impl Default for Pagination {
     fn default() -> Self {
         Self {
-            offset: 0,
-            limit: QUERY_DEFAULT_LIMIT,
+            offset: Some(0),
+            limit: Some(QUERY_DEFAULT_LIMIT),
         }
     }
 }
 
-// ParsePagination validate PageRequest and returns page number & limit.
-pub fn parse_pagination(Pagination { offset, mut limit }: Pagination) -> (u32, u8) {
+// ParsePagination validate Pagination and returns page number & limit.
+pub fn parse_pagination(pagination: Pagination) -> (u32, u8) {
+    let offset = pagination.offset.unwrap_or(0);
+    let mut limit = pagination.limit.unwrap_or(QUERY_DEFAULT_LIMIT);
+
     if limit == 0 {
         limit = QUERY_DEFAULT_LIMIT
     }
@@ -51,22 +51,25 @@ mod tests {
     #[test]
     fn parse_pagination_works() {
         let (page, limit) = parse_pagination(Pagination {
-            offset: 100,
-            limit: 30,
+            offset: Some(100),
+            limit: Some(30),
         });
 
         assert_eq!(page, 4);
         assert_eq!(limit, 30);
 
         let (page, limit) = parse_pagination(Pagination {
-            offset: 100,
-            limit: 0,
+            offset: Some(100),
+            limit: Some(0),
         });
 
         assert_eq!(page, 2);
         assert_eq!(limit, 100);
 
-        let (page, limit) = parse_pagination(Pagination::default());
+        let (page, limit) = parse_pagination(Pagination {
+            offset: None,
+            limit: None,
+        });
 
         assert_eq!(page, 1);
         assert_eq!(limit, 100);
