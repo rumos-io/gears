4d733945049342fda158ad82905ecb2ac64c7b32
NYBACHOK <nybachokua@gmail.com>
2024-07-18 20:32:13 +0300

    delete completely

--- x/auth/src/lib.rs
+++ x/auth/src/lib.rs
@@ -2,14 +2,12 @@ mod abci_handler;
 mod client;
 mod genesis;
 mod keeper;
-mod message;
 mod params;
 
 pub use abci_handler::*;
 pub use client::*;
 pub use genesis::*;
 pub use keeper::*;
-pub use message::*;
 pub use params::*;
 
 //
