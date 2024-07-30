3c73657bbc9893061297a4f2b6c1679c1f7769ad
NYBACHOK <nybachokua@gmail.com>
2024-07-30 10:47:47 +0300

    some usage in slashing

--- x/slashing/src/types/query.rs
+++ x/slashing/src/types/query.rs
@@ -1,8 +1,9 @@
 use gears::{
     core::{errors::CoreError, query::request::PageRequest},
-    derive::Query,
+    derive::{Protobuf, Query, Raw},
+    tendermint::types::proto::Protobuf,
     types::{
-        address::{AddressError, ConsAddress},
+        address::ConsAddress,
         pagination::{request::PaginationRequest, response::PaginationResponse},
     },
 };
@@ -15,40 +16,14 @@ use crate::{SlashingParams, SlashingParamsRaw, ValidatorSigningInfo, ValidatorSi
 // Requests
 // =====
 
-#[derive(Clone, PartialEq, Serialize, Deserialize, Message)]
-pub struct QuerySigningInfoRequestRaw {
-    #[prost(bytes)]
-    pub cons_address: Vec<u8>,
-}
-
-impl From<QuerySigningInfoRequest> for QuerySigningInfoRequestRaw {
-    fn from(value: QuerySigningInfoRequest) -> Self {
-        Self {
-            cons_address: value.cons_address.into(),
-        }
-    }
-}
-
 /// QuerySigningInfoRequest is the request type for the Query/SigningInfo RPC
 /// method
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Query)]
-#[query(
-    raw = "QuerySigningInfoRequestRaw",
-    url = "/cosmos.slashing.v1beta1.Query/SigningInfo"
-)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Raw, Protobuf, Query)]
+#[query(url = "/cosmos.slashing.v1beta1.Query/SigningInfo")]
 pub struct QuerySigningInfoRequest {
     /// cons_address is the address to query signing info of
+    #[raw(raw = Vec::<u8>, kind(bytes),  )]
     pub cons_address: ConsAddress,
-}
-
-impl TryFrom<QuerySigningInfoRequestRaw> for QuerySigningInfoRequest {
-    type Error = AddressError;
-
-    fn try_from(value: QuerySigningInfoRequestRaw) -> Result<Self, Self::Error> {
-        Ok(QuerySigningInfoRequest {
-            cons_address: ConsAddress::try_from(value.cons_address)?,
-        })
-    }
 }
 
 #[derive(Clone, PartialEq, Message)]
@@ -69,10 +44,7 @@ impl From<QuerySigningInfosRequest> for QuerySigningInfosRequestRaw {
 /// QuerySigningInfosRequest is the request type for the Query/SigningInfos RPC
 /// method
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Query)]
-#[query(
-    raw = "QuerySigningInfosRequestRaw",
-    url = "/cosmos.slashing.v1beta1.Query/SigningInfos"
-)]
+#[query(url = "/cosmos.slashing.v1beta1.Query/SigningInfos")]
 pub struct QuerySigningInfosRequest {
     /// pagination defines an optional pagination for the request.
     pub pagination: PaginationRequest,
@@ -94,11 +66,10 @@ impl TryFrom<QuerySigningInfosRequestRaw> for QuerySigningInfosRequest {
     }
 }
 
-#[derive(Clone, PartialEq, Message, Query)]
-#[query(
-    raw = "QueryParamsRequest",
-    url = "/cosmos.slashing.v1beta1.Query/Params"
-)]
+impl Protobuf<QuerySigningInfosRequestRaw> for QuerySigningInfosRequest {}
+
+#[derive(Clone, PartialEq, Message, Query, Raw, Protobuf)]
+#[query(url = "/cosmos.slashing.v1beta1.Query/Params")]
 pub struct QueryParamsRequest {}
 
 // =====
@@ -122,7 +93,6 @@ impl From<QuerySigningInfoResponse> for QuerySigningInfoResponseRaw {
 /// QuerySigningInfoResponse is the response type for the Query/SigningInfo RPC
 /// method
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Query)]
-#[query(raw = "QuerySigningInfoResponseRaw")]
 pub struct QuerySigningInfoResponse {
     /// val_signing_info is the signing info of requested val cons address
     pub val_signing_info: ValidatorSigningInfo,
@@ -142,6 +112,8 @@ impl TryFrom<QuerySigningInfoResponseRaw> for QuerySigningInfoResponse {
         })
     }
 }
+
+impl Protobuf<QuerySigningInfoResponseRaw> for QuerySigningInfoResponse {}
 
 #[derive(Clone, PartialEq, Message)]
 pub struct QuerySigningInfosResponseRaw {
@@ -163,7 +135,6 @@ impl From<QuerySigningInfosResponse> for QuerySigningInfosResponseRaw {
 /// QuerySigningInfosResponse is the response type for the Query/SigningInfos RPC
 /// method
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize, Query)]
-#[query(raw = "QuerySigningInfosResponseRaw")]
 pub struct QuerySigningInfosResponse {
     /// Info is the signing info of all validators
     pub info: Vec<ValidatorSigningInfo>,
@@ -186,6 +157,8 @@ impl TryFrom<QuerySigningInfosResponseRaw> for QuerySigningInfosResponse {
     }
 }
 
+impl Protobuf<QuerySigningInfosResponseRaw> for QuerySigningInfosResponse {}
+
 /// QueryParamsResponse is the response type for the Query/Params RPC method
 #[derive(Clone, Serialize, Message)]
 pub struct QueryParamsResponseRaw {
@@ -203,7 +176,6 @@ impl From<QueryParamsResponse> for QueryParamsResponseRaw {
 
 /// QueryParamsResponse is the response type for the Query/Params RPC method
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Query)]
-#[query(raw = "QueryParamsResponseRaw")]
 pub struct QueryParamsResponse {
     pub params: SlashingParams,
 }
@@ -222,3 +194,5 @@ impl TryFrom<QueryParamsResponseRaw> for QueryParamsResponse {
         })
     }
 }
+
+impl Protobuf<QueryParamsResponseRaw> for QueryParamsResponse {}
