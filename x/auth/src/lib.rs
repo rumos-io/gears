3a65eb484de67f8236deb5f7227b5ef2dbffaf40
NYBACHOK <nybachokua@gmail.com>
2024-07-30 14:15:13 +0300

    update auth xmod

--- x/auth/src/lib.rs
+++ x/auth/src/lib.rs
@@ -1,23 +1,21 @@
-// mod abci_handler;
-// mod client;
-// mod genesis;
-// mod keeper;
-// mod params;
+mod abci_handler;
+mod client;
+mod genesis;
+mod keeper;
+mod params;
 
-// pub use abci_handler::*;
-// pub use client::*;
-// pub use genesis::*;
-// pub use keeper::*;
-// pub use params::*;
+pub use abci_handler::*;
+pub use client::*;
+pub use genesis::*;
+pub use keeper::*;
+pub use params::*;
 
-// //
+pub fn new_module_addr(module_name: &str) -> gears::types::address::AccAddress {
+    use sha2::Digest;
+    let hash = sha2::Sha256::digest(module_name);
+    // sdk behavior. It gets slice of first 20 bytes from sha256 hash
+    let addr_bytes = &hash[..20];
 
-// pub fn new_module_addr(module_name: &str) -> gears::types::address::AccAddress {
-//     use sha2::Digest;
-//     let hash = sha2::Sha256::digest(module_name);
-//     // sdk behavior. It gets slice of first 20 bytes from sha256 hash
-//     let addr_bytes = &hash[..20];
-
-//     gears::types::address::AccAddress::try_from(addr_bytes.to_vec())
-//         .expect("vector of 20 bytes can't produce error because 0 < 20 < MAX_ADDR_LEN")
-// }
+    gears::types::address::AccAddress::try_from(addr_bytes.to_vec())
+        .expect("vector of 20 bytes can't produce error because 0 < 20 < MAX_ADDR_LEN")
+}
