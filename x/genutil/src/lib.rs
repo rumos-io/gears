bfb3f5b4a17e2033a3535e5ef29b075e41e7818c
NYBACHOK <nybachokua@gmail.com>
2024-08-15 16:43:35 +0300

    init

---
+++ x/genutil/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod keeper;
+pub mod types;
+pub mod cli;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
