5f261593189eed22080717cc373a3dbced82339c
NYBACHOK <nybachokua@gmail.com>
2024-08-19 13:03:53 +0300

    slightly finish

--- x/genutil/src/keeper.rs
+++ x/genutil/src/keeper.rs
@@ -1,9 +1,30 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
-use gears::types::tx::Tx;
+use gears::types::{address::AccAddress, base::coins::UnsignedCoins, tx::Tx};
 use staking::CreateValidator;
 
-pub fn collect_txs(dir: impl AsRef<Path>) -> anyhow::Result<()> {
+// pub trait BankKeeper<SK: StoreKey, M: Module> {
+//     fn all_balances<DB: Database, CTX: QueryableContext<DB, SK>>(
+//         &self,
+//         ctx: &CTX,
+//         addr: AccAddress,
+//     ) -> Result<Vec<UnsignedCoin>, GasStoreErrors>;
+// }
+
+#[derive(Debug, Clone)]
+pub struct GenesisBalance {
+    pub address: AccAddress,
+    pub coins: UnsignedCoins,
+}
+
+// <SK: StoreKey, M: Module, DB: Database, CTX: QueryableContext<DB, SK>>
+pub fn collect_txs(
+    dir: impl AsRef<Path>,
+    moniker: String,
+    balance: impl IntoIterator<Item = (AccAddress, GenesisBalance)>,
+) -> anyhow::Result<String> {
+    let balance = balance.into_iter().collect::<HashMap<_, _>>();
+
     let items = if dir.as_ref().is_dir() {
         let mut entities = Vec::new();
 
@@ -32,12 +53,18 @@ pub fn collect_txs(dir: impl AsRef<Path>) -> anyhow::Result<()> {
         vec![tx]
     };
 
+    let mut addresses_ip = Vec::with_capacity(items.len());
     for tx in items {
-        if tx.get_msgs().len() != 1 {
+        let msg = tx.get_msgs();
+        if msg.len() != 1 {
             Err(anyhow::anyhow!(
                 "genesis transactions must be single-message"
             ))?
         }
+
+        // the memo flag is used to store
+        // the ip and node-id, for example this may be:
+        // "528fd3df22b31f4969b05652bfe8f0fe921321d5@192.168.2.37:26656"
 
         let node_id_addr = tx.get_memo();
         if node_id_addr.is_empty() {
@@ -45,7 +72,37 @@ pub fn collect_txs(dir: impl AsRef<Path>) -> anyhow::Result<()> {
                 "failed to find node's address and IP due empty memo"
             ))?
         }
+
+        let msg = msg.first();
+        let delegator_balance = balance
+            .get(&msg.delegator_address)
+            .ok_or(anyhow::anyhow!("account balance not in genesis state"))?;
+
+        let _ = balance
+            .get(&msg.validator_address.clone().into())
+            .ok_or(anyhow::anyhow!("account balance not in genesis state"))?;
+
+        if delegator_balance.coins.amount_of(&msg.value.denom) < msg.value.amount {
+            Err(anyhow::anyhow!("insufficient fund for delegation"))?
+        }
+
+        // exclude itself from persistent peers
+        if msg.description.moniker != moniker {
+            addresses_ip.push(format!("{node_id_addr},"))
+        }
     }
 
-    Ok(())
+    let lenght = addresses_ip
+        .iter()
+        .fold(0, |accumulator, this| accumulator + this.len());
+
+    let peers =
+        addresses_ip
+            .into_iter()
+            .fold(String::with_capacity(lenght), |mut accumulator, this| {
+                accumulator.extend(this.chars());
+                accumulator
+            });
+
+    Ok(peers)
 }
