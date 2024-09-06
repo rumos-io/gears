6045bf96d23f27fa5f8897bfa5b807cb1c47ce9e
NYBACHOK <nybachokua@gmail.com>
2024-09-06 13:22:34 +0300

    add empty check for env

--- address/build.rs
+++ address/build.rs
@@ -1,6 +1,9 @@
 use std::{env, error::Error};
 fn main() -> Result<(), Box<dyn Error>> {
     let account_address_prefix = env::var("BECH_32_MAIN_PREFIX").map_err(|_| "BECH_32_MAIN_PREFIX environment variable must be set. This is best done in a .cargo/config.toml file in the root of your project")?;
+    if account_address_prefix.trim().is_empty() {
+        Err("BECH_32_MAIN_PREFIX environment variable can't be empty")?
+    }
     println!(
         "cargo:rustc-env=BECH_32_MAIN_PREFIX={}",
         account_address_prefix
