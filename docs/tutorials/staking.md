005344a305e4783c662361f0235596bf80fa2977
Kevin Jones <kevin.jonesat@gmail.com>
2024-09-08 19:02:08 +0100

    Use gentx in init script

--- docs/tutorials/staking.md
+++ docs/tutorials/staking.md
@@ -1,8 +1,10 @@
 # Staking tutorial
 
+## Adding validators
+
 1. Create a validator
 
-```
+```shell
 gaiad tx staking create-validator \
   --amount=5uatom \
   --pubkey='{"@type":"/cosmos.crypto.ed25519.PubKey","key":"+uo5x4+nFiCBt2MuhVwT5XeMfj6ttkjY/JC6WyHb+rE="}' \
@@ -18,6 +20,16 @@ gaiad tx staking create-validator \
   -y
 ```
 
-```
+```shell
 gaiad query staking validators
+```
+
+## Genesis validator
+
+```shell
+gaia-rs gentx 10uatom --from-key alice --account-number 2 --sequence 0 --keyring-backend=test
+```
+
+```shell
+gaia-rs collect-gentxs
 ```
--- gaia-rs/scripts/init.sh
+++ gaia-rs/scripts/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-amount=34
+amount=1000000000000
 if [[ $AMOUNT != "" ]]; then
   amount=$AMOUNT
 fi
@@ -10,4 +10,12 @@ set -eux
 rm -rf ~/.gaia-rs
 cargo run -- init test
 
+echo "Generating deterministic account - alice"
+echo "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow" | gaia-rs keys add alice --recover --keyring-backend=test
+
 cargo run -- add-genesis-account cosmos1syavy2npfyt9tcncdtsdzf7kny9lh777pahuux "$amount"uatom
+
+echo "Collecting genesis txs..."
+cargo run -- gentx 10uatom --from-key alice --account-number 2 --sequence 0 --keyring-backend=test
+
+cargo run -- collect-gentxs
