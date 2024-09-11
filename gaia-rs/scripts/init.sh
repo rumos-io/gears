55fe5dfa2f6ea13a5e425d67d69588d3a747caf4
Kevin Jones <kevin.jonesat@gmail.com>
2024-09-11 11:50:47 +0100

    Hard code values in init script

--- gaia-rs/scripts/init.sh
+++ gaia-rs/scripts/init.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-amount=1000000000000
-if [[ $AMOUNT != "" ]]; then
-  amount=$AMOUNT
-fi
-
 set -eux
 
 rm -rf ~/.gaia-rs
@@ -13,7 +8,7 @@ cargo run -- init test
 echo "Generating deterministic account - alice"
 echo "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow" | gaia-rs keys add alice --recover --keyring-backend=test
 
-cargo run -- add-genesis-account cosmos1syavy2npfyt9tcncdtsdzf7kny9lh777pahuux "$amount"uatom
+cargo run -- add-genesis-account cosmos1syavy2npfyt9tcncdtsdzf7kny9lh777pahuux 1000000000000uatom
 
 cargo run -- gentx 10000000000uatom --from-key alice --min-self-delegation 1 --moniker test --account-number 0 --sequence 0 --keyring-backend=test
 
