fdf136d2065ef3bf409c037dbc622da8b1ccbf92
NYBACHOK <nybachokua@gmail.com>
2024-09-13 12:58:17 +0300

    edit addr for module

--- gaia-rs/src/modules.rs
+++ gaia-rs/src/modules.rs
@@ -19,12 +19,7 @@ impl Module for GaiaModules {
 
     fn get_address(&self) -> AccAddress {
         match self {
-            GaiaModules::FeeCollector => {
-                //TODO: construct address from Vec<u8> + make address constant
-                //TODO: where do these addresses come from?
-                AccAddress::from_bech32("cosmos17xpfvakm2amg962yls6f84z3kell8c5lserqta")
-                    .expect("hard coded address is valid")
-            }
+            GaiaModules::FeeCollector => auth::new_module_addr(&self.get_name()),
             GaiaModules::BondedPool => auth::new_module_addr(&self.get_name()),
             GaiaModules::NotBondedPool => auth::new_module_addr(&self.get_name()),
         }
