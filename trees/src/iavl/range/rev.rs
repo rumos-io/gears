caa851bffb0807f94a7d7d13f87fb355e4cbc01d
NYBACHOK <nybachokua@gmail.com>
2024-10-10 11:39:48 +0300

    redability

--- trees/src/iavl/range/rev.rs
+++ trees/src/iavl/range/rev.rs
@@ -19,48 +19,23 @@ pub struct RevRange<'a, DB, RB, R> {
 
 impl<DB: Database, R: RangeBounds<RB>, RB: AsRef<[u8]> + Debug> RevRange<'_, DB, RB, R> {
     fn traverse(&mut self) -> Option<(Vec<u8>, Vec<u8>)> {
-        // Instead usage of VecDeque I assume Vec already rev in `new`(or similar)
-        let node = self.delayed_nodes.pop_front()?;
+        let node = self.delayed_nodes.pop_back()?;
 
         let after_start = match self.range.start_bound() {
-            Bound::Included(l) => {
-                // dbg!(l.as_ref(), node.get_key(), node.get_key() >= l.as_ref());
-                node.get_key() > l.as_ref()
-            }
+            Bound::Included(l) => node.get_key() >= l.as_ref(),
             Bound::Excluded(l) => node.get_key() > l.as_ref(),
             Bound::Unbounded => true,
         };
 
         let before_end = match self.range.end_bound() {
             Bound::Included(u) => node.get_key() <= u.as_ref(),
-            Bound::Excluded(u) => {
-                // dbg!(u.as_ref(), node.get_key(), node.get_key() < u.as_ref());
-                node.get_key() < u.as_ref()
-            }
+            Bound::Excluded(u) => node.get_key() < u.as_ref(),
             Bound::Unbounded => true,
         };
 
         match *node {
             Node::Leaf(leaf) => {
-                // TODO: Replace with (after_start && before_end) when add tests
-                let is_contains = (match self.range.start_bound() {
-                    Bound::Included(start) => start.as_ref() <= &leaf.key,
-                    Bound::Excluded(start) => start.as_ref() < &leaf.key,
-                    Bound::Unbounded => true,
-                }) && (match self.range.end_bound() {
-                    Bound::Included(end) => leaf.key.as_slice() <= end.as_ref(),
-                    Bound::Excluded(end) => leaf.key.as_slice() < end.as_ref(),
-                    Bound::Unbounded => true,
-                });
-
-                // dbg!(
-                //     is_contains,
-                //     self.range.start_bound(),
-                //     self.range.end_bound(),
-                //     leaf.key.as_slice()
-                // );
-
-                if is_contains {
+                if after_start && before_end {
                     // we have a leaf node within the range
                     return Some((leaf.key, leaf.value));
                 }
@@ -68,31 +43,27 @@ impl<DB: Database, R: RangeBounds<RB>, RB: AsRef<[u8]> + Debug> RevRange<'_, DB,
             Node::Inner(inner) => {
                 // Traverse through the right subtree, then the left subtree.
                 if before_end {
-                    match inner.left_node {
-                        Some(left_node) => self.delayed_nodes.push_front(left_node),
-                        None => {
-                            let left_node = self
-                                .node_db
-                                .get_node(&inner.right_hash)
-                                .expect("node db should contain all nodes");
+                    let left_node = match inner.left_node {
+                        Some(left_node) => left_node,
+                        None => self
+                            .node_db
+                            .get_node(&inner.right_hash)
+                            .expect("node db should contain all nodes"),
+                    };
 
-                            self.delayed_nodes.push_back(left_node);
-                        }
-                    }
+                    self.delayed_nodes.push_front(left_node)
                 }
 
                 if after_start {
-                    match inner.right_node {
-                        Some(right_node) => self.delayed_nodes.push_front(right_node),
-                        None => {
-                            let right_node = self
-                                .node_db
-                                .get_node(&inner.left_hash)
-                                .expect("node db should contain all nodes");
+                    let right_node = match inner.right_node {
+                        Some(right_node) => right_node,
+                        None => self
+                            .node_db
+                            .get_node(&inner.left_hash)
+                            .expect("node db should contain all nodes"),
+                    };
 
-                            self.delayed_nodes.push_back(right_node);
-                        }
-                    }
+                    self.delayed_nodes.push_front(right_node)
                 }
             }
         }
